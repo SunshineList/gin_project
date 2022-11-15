00ed0052db36624d630069eae9ca1bd581906484
wangzw <wzw71603974@qq.com>
2022-11-15 17:56:58 +0800

    fix: jwt中间件使用问题

--- middleware/jwt_moddleware.go
+++ middleware/jwt_moddleware.go
@@ -9,24 +9,27 @@ import (
 
 // 基于JWT的认证中间件
 
-func JWTAuthMiddleware(context *gin.Context) {
-	// 从请求头中取出
-	signToken := context.Request.Header.Get("Authorization")
-	if signToken == "" {
-		response.FailAndMsg("token为空", context)
-		context.Abort()
-		return
+func JWTAuthMiddleware() gin.HandlerFunc {
+	return func(context *gin.Context) {
+		// 从请求头中取出
+		signToken := context.Request.Header.Get("Authorization")
+		if signToken == "" {
+			response.FailAndMsg("token为空", context)
+			context.Abort()
+			return
+		}
+		// 校验token
+		myclaims, err := utils.ParserToken(signToken)
+		if err != nil {
+			fmt.Println(err)
+			response.FailAndMsg("token校验失败", context)
+			context.Abort()
+			return
+		}
+		// 将用户的id放在到请求的上下文中
+		context.Set("user_id", myclaims.Id)
+		context.Next() // 后续的处理函数可以用过context.Get("userid")来获取当前请求的id
+
 	}
-	// 校验token
-	myclaims, err := utils.ParserToken(signToken)
-	if err != nil {
-		fmt.Println(err)
-		response.FailAndMsg("token校验失败", context)
-		context.Abort()
-		return
-	}
-	// 将用户的id放在到请求的上下文中
-	context.Set("user_id", myclaims.Id)
-	context.Next() // 后续的处理函数可以用过c.Get("userid")来获取当前请求的id
 
 }
